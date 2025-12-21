aade99af512d97a40029fd648124afa2ebab760e
LakshyaSingh07 <lakshyac99@gmail.com>
2025-12-21 09:45:14 +0530

    changed the bg clr, footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -170,7 +170,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 pt-8 text-center text-gray-400">
-          <p>&copy; 2025 BrandSync. All rights reserved.</p>
+          <p>&copy; 2025 KoDude Technology Pvt. Ltd. All rights reserved.</p>
         </div>
       </div>
     </footer>
